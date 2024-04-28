523ca243c88f6488cc808283819936814fa0c727
Orlando J. Vilau <ojvilau@gmail.com>
2024-04-28 04:45:09 +0100

    more examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 
 export default function App() {
   return (
@@ -16,18 +17,22 @@ export default function App() {
             <li>
               <Link to="/users">Users</Link>
             </li>
+            {[1, 2, 3, 4, 5].map((id) => (
+              <li>
+                <Link to={`/users/${id}`}>User {id}</Link>
+              </li>
+            ))}
           </ul>
         </nav>
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
+          <Route
+            path="/about"
+            render={(props) => <About location={props.location} />}
+          />
+          <Route path={["/users/:id", "/users"]} component={Users} />
           <Route path="/">
             <Home />
           </Route>
@@ -41,12 +46,19 @@ function Home() {
   return <h2>Home</h2>;
 }
 
-function About() {
-  return <h2>About</h2>;
+function About({ location }) {
+  return <h2>About ({location.pathname})</h2>;
 }
 
 function Users() {
-  const [count, setCount] = useState(0);
+  const params = useParams();
+  const id = +params.id;
+  const [count, setCount] = useState(Number.isNaN(id) ? 0 : id);
+
+  useEffect(() => {
+    setCount(Number.isNaN(id) ? 0 : id);
+  }, [id]);
+
   return (
     <>
       <h2>Users ({count})</h2>
